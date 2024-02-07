8e220958c029fe61724033279f6fbb343b458728
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 15:04:09 +0300

    some ahgkdjklsjf

--- email.js
+++ email.js
@@ -1,4 +1,3 @@
-//  this email to the user
 const dynamicTemplateData = {
     url: emailData.url,
     greeting:
@@ -7,8 +6,8 @@ const dynamicTemplateData = {
             : `عزيزي ${emailData.userName}،`,
     content1:
         emailData.language === 'en'
-            ? 'Your project became available again!'
-            : '!مشروعك أصبح متاحاً مرة أخرى',
+            ? 'Your project is sold.'
+            : 'مشروعك متاح مرة أخرى.',
     content2:
         emailData.language === 'en'
             ? `Project name: ${emailData.project_name}`
@@ -16,7 +15,16 @@ const dynamicTemplateData = {
     button_text:
         emailData.language === 'en' ? 'View Project' : 'عرض المشروع',
     subject:
+        emailData.language === 'en' ? 'Project Sold' : 'المشروع متاح'
+};
+// msg to the user
+const msg = {
+    to: emailData.userEmail,
+    from: { email: this.from, name: 'Fozdeal' },
+    dynamicTemplateData,
+    templateId:
         emailData.language === 'en'
-            ? 'Project Available Again'
-            : 'مشروعك متاح مجدّداً'
+            ? this.templateIds.content2ButtonEn
+            : this.templateIds.content2ButtonAr
+};      : 'مشروعك متاح مجدّداً'
 };
