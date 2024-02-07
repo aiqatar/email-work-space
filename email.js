a2daa96c40891f771854f4c140568742ec57a813
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 12:46:21 +0300

    email

--- email.js
+++ email.js
@@ -1,16 +1,24 @@
 
 
-hwllow how are you
- 
-
-all done how do ou do
 
 
 
-
-
-
-
+const dynamicTemplateData = {
+    url: `https://fozdeal.com/user/verify/email/${hash}`,
+    greeting:
+        emailData.language === 'en'
+            ? `Dear ${name},`
+            : `عزيزي ${name}،`,
+    content1:
+        emailData.language === 'en'
+            ? 'After verifying your email, the journey on FOZDEAL starts. Please click here:'
+            : 'ستبدأ رحلتك مع فوزديل بعد التحقق من بريدك الإلكتروني. فضلاً انقر هنا:',
+    button_text: emailData.language === 'en' ? 'Verify' : 'تحقق',
+    subject:
+        emailData.language === 'en'
+            ? 'Verify Your Account'
+            : 'تفعيل الحساب'
+};
 
 
 
