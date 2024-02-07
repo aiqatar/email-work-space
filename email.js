36572cc2472647b86ca7ba4a917e607d73147684
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 15:20:14 +0300

    SOMED

--- email.js
+++ email.js
@@ -1,14 +1,38 @@
-const Adminmsg = {
+// USER
+const dynamicTemplateData = {
+    url: emailData.url,
+    greeting:
+        emailData.language === 'en'
+            ? `Dear ${name},`
+            : `عزيزي ${name}،`,
+    content1:
+        emailData.language === 'en'
+            ? `Your project has been Featured for ${emailData.no_of_days} days.`
+            : `لقد تم تمييز مشروعك لمدة ${emailData.no_of_days} يومًا.`,
+    content2:
+        emailData.language === 'en'
+            ? `Project name: ${emailData.project_name}`
+            : `اسم المشروع: ${emailData.project_name}`,
+    button_text:
+        emailData.language === 'en' ? 'View Project' : 'عرض المشروع',
+    subject:
+        emailData.language === 'en'
+            ? 'Project Featured'
+            : 'تم تمييز المشروع'
+};
+
+
+// ADMIN
+const AdminMsg = {
     to: this.adminEmail,
-    from: this.from,
+    from: { email: this.from, name: 'FozDeal' },
     dynamicTemplateData: {
         url: emailData.url,
-        greeting: `Dear Admin,`,
-        content1: `The following project is set to: Sold`,
+        greeting: `Hi Admin,`,
+        content1: `The following project is Featured for ${emailData.no_of_days} days:`,
         content2: `Project name: ${emailData.project_name}`,
-        content3: `Project owner: ${emailData.first_name} ${emailData.last_name}`,
-        button_text: 'View Details',
-        subject: 'A Project Has Been Sold'
+        button_text: 'View Project',
+        subject: 'Project Featured'
     },
-    templateId: this.templateIds.content3ButtonEn
+    templateId: this.templateIds.content2ButtonEn
 };
