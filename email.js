7e82b6b5c213b4d2cf078fcc662418f3f3bada65
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 13:13:11 +0300

    kinda a

--- email.js
+++ email.js
@@ -48,8 +48,8 @@ if (emailData.project_status === 'approved') {
 if (emailData.project_status === 'rejected') {
     dynamicTemplateData.content1 =
         emailData.language === 'en'
-            ? "After a careful review, we regret to inform you that your project can't be displayed on FOZDEAL due to the following reason(s):"
-            : 'شكرًا لتقديم مشروعك. بعد استعراض دقيق، نأسف لإبلاغكم بأن مشروعك لم يتم قبوله في هذا الوقت. نظرًا للأسباب التالية:';
+            ? 'After a careful review, we regret to inform you that your project can't be displayed on FOZDEAL due to the following reason(s):'
+            : '';
 
     dynamicTemplateData.content3 =
         emailData.language === 'en'
