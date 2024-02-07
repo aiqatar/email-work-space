5bdb92315492fb209469e901ddb7397445d16fa3
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 12:52:27 +0300

    SOME

--- email.js
+++ email.js
@@ -2,25 +2,31 @@
 
 
 
-
 const dynamicTemplateData = {
-    url: `https://fozdeal.com/user/verify/email/${hash}`,
+    url: `https://fozdeal.com/projects?user_id=${user_id}&project_status=all`,
     greeting:
         emailData.language === 'en'
             ? `Dear ${name},`
-            : `عزيزي ${name}،`,
+            : `,${name} مرحبا`,
     content1:
         emailData.language === 'en'
-            ? 'After verifying your email, the journey on FOZDEAL starts. Please click here:'
-            : 'ستبدأ رحلتك في فوزديل بعد التحقق من بريدك الإلكتروني. فضلاً انقر هنا:',
-    button_text: emailData.language === 'en' ? 'Verify' : 'تحقق',
+            ? `After reviewing your project, we kindly request the following modifications:`
+            : ':بعد مراجعتنا لمشروعك، نرجو أن تُجري التعديلات التالية',
+    content2: note,
+    content3:
+        emailData.language === 'en'
+            ? `Click on the following button given below to view your project status.`
+            : 'انقر فوق الزر أدناه لعرض حالة مشروعك',
+    button_text:
+        emailData.language === 'en' ? 'View Project' : 'عرض المشروع',
     subject:
         emailData.language === 'en'
-            ? 'Verify Your Account'
-            : 'التحقق من البريد'
+            ? 'Modification Required'
+            : 'طلب تعديل',
+
+    content4: '',
+    content5: ''
 };
-
-
 
 
 
