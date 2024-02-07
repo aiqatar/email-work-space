92d0397efc687c4fed1ddbbc19e5bc80a7997298
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 13:57:08 +0300

    hii

--- email.js
+++ email.js
@@ -1,11 +1,24 @@
 
+
 const dynamicTemplateData = {
-    url: emailData.url,
-    greeting: `Dear Admin,`,
-    content1: `The following project has been set as: Sold.`,
-    content2: `Project name: ${emailData.project_name}`,
-    content3: `Project owner:`
-        button_text: 'View Details',
-    subject: 'Sold Project'
+    url: `https://fozdeal.com/projects/${project_id}`,
+    greeting:
+        emailData.language === 'en'
+            ? `Dear ${first_name},`
+            : `عزيزي ${first_name}،`,
+    content1:
+        emailData.language === 'en'
+            ? `Thank you for paying (QR${amount}) to upload your project on FOZDEAL
+     platform. We’ll contact you once it gets reviewed.`
+            : `نشكرك على دفع ${amount} ريال قطري لعرض مشروعك في منصة فوزديل. سيتم التواصل معك بعد إتمام مراجعته`,
+    content2:
+        emailData.language === 'en'
+            ? `You can track your project status from here:`
+            : 'يمكنك متابعة حالة مشروعك من هنا',
+    button_text:
+        emailData.language === 'en' ? 'Project Status' : 'حالة المشروع',
+    subject:
+        emailData.language === 'en' ? 'Project Status' : 'حالة المشروع'
 };
+
 
