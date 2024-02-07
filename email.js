4eed28f4ab722a8105e4cdbe2d0dc11deda03cf6
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 12:51:40 +0300

    Kinda verify email

--- email.js
+++ email.js
@@ -12,12 +12,12 @@ const dynamicTemplateData = {
     content1:
         emailData.language === 'en'
             ? 'After verifying your email, the journey on FOZDEAL starts. Please click here:'
-            : 'ستبدأ رحلتك مع فوزديل بعد التحقق من بريدك الإلكتروني. فضلاً انقر هنا:',
+            : 'ستبدأ رحلتك في فوزديل بعد التحقق من بريدك الإلكتروني. فضلاً انقر هنا:',
     button_text: emailData.language === 'en' ? 'Verify' : 'تحقق',
     subject:
         emailData.language === 'en'
             ? 'Verify Your Account'
-            : 'تفعيل الحساب'
+            : 'التحقق من البريد'
 };
 
 
