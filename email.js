c2f18301a4bce2e8f17d49329d2af7f1ad4303a6
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 15:15:09 +0300

    kinda sold user

--- email.js
+++ email.js
@@ -6,8 +6,8 @@ const dynamicTemplateData = {
             : `عزيزي ${emailData.userName}،`,
     content1:
         emailData.language === 'en'
-            ? 'Your project is sold.'
-            : 'مشروعك متاح مرة أخرى.',
+            ? 'Your project is set to be: sold. You can contact us if you want to change that.'
+            : 'تم تغيير حالة مشروعك إلى: مُباع. يمكنك التواصل معنا إذا رغبت في تغيير ذلك',
     content2:
         emailData.language === 'en'
             ? `Project name: ${emailData.project_name}`
@@ -15,5 +15,5 @@ const dynamicTemplateData = {
     button_text:
         emailData.language === 'en' ? 'View Project' : 'عرض المشروع',
     subject:
-        emailData.language === 'en' ? 'Project Sold' : 'المشروع متاح'
+        emailData.language === 'en' ? 'Project Set to Sold' : ' حالة المشروع - مُباع '
 };
