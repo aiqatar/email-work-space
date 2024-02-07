ed9e1300afa8c92f561293142520c364ae91fdcf
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 14:51:20 +0300

    sldjk

--- email.js
+++ email.js
@@ -1,15 +1,4 @@
 
-//project sold
-const dynamicTemplateData = {
-    url: emailData.url,
-    greeting: `Dear Admin,`,
-    content1: `The following project has been set as: Sold.`,
-    content2: `Project name: ${emailData.project_name}`,
-    content3: `Project owner: ${emailData.name}`,
-    button_text: 'View Details',
-    subject: 'Sold Project'
-};
-
 
 
 //  this email to the user
