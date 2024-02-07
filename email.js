39a5a3a35921fcb8dd41ddb9c6993bbe8996b5f5
aiqatar <136283371+aiqatar@users.noreply.github.com>
2024-02-07 14:50:53 +0300

    some hkldf

--- email.js
+++ email.js
@@ -1,24 +1,51 @@
 
-
+//project sold
 const dynamicTemplateData = {
-    url: `https://fozdeal.com/projects/${project_id}`,
-    greeting:
-        emailData.language === 'en'
-            ? `Dear ${first_name},`
-            : `عزيزي ${first_name}،`,
-    content1:
-        emailData.language === 'en'
-            ? `Thank you for paying the amount of (QR${amount}) to upload your project on FOZDEAL
-     platform. We’ll contact you once it gets reviewed.`
-            : `نشكرك على دفع مبلغ ${amount} ريال قطري لعرض مشروعك في منصة فوزديل. سيتم التواصل معك بعد إتمام مراجعته`,
-    content2:
-        emailData.language === 'en'
-            ? `You can track your project status from here:`
-            : 'يمكنك متابعة حالة مشروعك من هنا',
-    button_text:
-        emailData.language === 'en' ? 'Project Status' : 'حالة المشروع',
-    subject:
-        emailData.language === 'en' ? 'Project Status' : 'حالة المشروع'
+    url: emailData.url,
+    greeting: `Dear Admin,`,
+    content1: `The following project has been set as: Sold.`,
+    content2: `Project name: ${emailData.project_name}`,
+    content3: `Project owner: ${emailData.name}`,
+    button_text: 'View Details',
+    subject: 'Sold Project'
 };
 
 
+
+//  this email to the user
+const dynamicTemplateData = {
+    url: emailData.url,
+    greeting:
+        emailData.language === 'en'
+            ? `Dear ${emailData.userName},`
+            : `عزيزي ${emailData.userName}،`,
+    content1:
+        emailData.language === 'en'
+            ? 'Your project is available again.'
+            : 'مشروعك متاح مرة أخرى.',
+    content2:
+        emailData.language === 'en'
+            ? `Project name: ${emailData.project_name}`
+            : `اسم المشروع: ${emailData.project_name}`,
+    button_text:
+        emailData.language === 'en' ? 'View Project' : 'عرض المشروع',
+    subject:
+        emailData.language === 'en'
+            ? 'Project Available'
+            : 'المشروع متاح'
+};
+
+// this email to the admin
+const AdminMsg = {
+    to: this.adminEmail,
+    from: { email: this.from, name: 'FozDeal' },
+    dynamicTemplateData: {
+        url: emailData.url,
+        greeting: `Hi Admin,`,
+        content1: `The following project is set to Available:`,
+        content2: `Project name: ${emailData.project_name}`,
+        button_text: 'View Project',
+        subject: 'Project Available'
+    },
+    templateId: this.templateIds.content2ButtonEn
+};
